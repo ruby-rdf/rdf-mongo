797e9cbb84c9cf2fbb126b1225f303e7b53dbd8d
Gregg Kellogg <gregg@greggkellogg.net>
2019-12-14 10:39:05 -0800

    Updates for 3.1 release and Ruby 2.7 calling sequences.

--- lib/rdf/mongo.rb
+++ lib/rdf/mongo.rb
@@ -118,13 +118,13 @@ module RDF
       ##
       # Initializes this repository instance.
       #
-      # @overload initialize(options = {}, &block)
+      # @overload initialize(**options, &block)
       #   @param  [Hash{Symbol => Object}] options
       #   @option options [String, #to_s] :title (nil)
       #   @option options [URI, #to_s]    :uri (nil)
       #     URI in the form `mongodb://host:port/db`. The URI should also identify the collection use, but appending a `collection` path component such as `mongodb://host:port/db/collection`, this ensures that the collection will be maintained if cloned. See [Mongo::Client options](https://docs.mongodb.org/ecosystem/tutorial/ruby-driver-tutorial-2-0/#uri-options-conversions) for more information on Mongo URIs.
       #
-      # @overload initialize(options = {}, &block)
+      # @overload initialize(**options, &block)
       #   @param  [Hash{Symbol => Object}] options
       #     See [Mongo::Client options](https://docs.mongodb.org/ecosystem/tutorial/ruby-driver-tutorial-2-0/#uri-options-conversions) for more information on Mongo Client options.
       #   @option options [String, #to_s] :title (nil)
@@ -136,7 +136,7 @@ module RDF
       #
       # @yield  [repository]
       # @yieldparam [Repository] repository
-      def initialize(options = {}, &block)
+      def initialize(**options, &block)
         collection = nil
         if options[:uri]
           options = options.dup
@@ -145,14 +145,14 @@ module RDF
           collection = coll || options.delete(:collection)
           db ||= "quadb"
           uri.path = "/#{db}" if coll
-          @client = ::Mongo::Client.new(uri.to_s, options)
+          @client = ::Mongo::Client.new(uri.to_s, **options)
         else
           warn "[DEPRECATION] RDF::Mongo::Repository#initialize expects a uri argument. Called from #{Gem.location_of_caller.join(':')}" unless options.empty?
           options[:database] ||= options.delete(:db) # 1.x compat
           options[:database] ||= 'quadb'
           hosts = Array(options[:host] || 'localhost')
           hosts.map! {|h| "#{h}:#{options[:port]}"} if options[:port]
-          @client = ::Mongo::Client.new(hosts, options)
+          @client = ::Mongo::Client.new(hosts, **options)
         end
 
         @collection = @client[options.delete(:collection) || 'quads']
@@ -258,8 +258,8 @@ module RDF
       # @private
       # @see RDF::Queryable#query_pattern
       # @see RDF::Query::Pattern
-      def query_pattern(pattern, options = {}, &block)
-        return enum_for(:query_pattern, pattern, options) unless block_given?
+      def query_pattern(pattern, **options, &block)
+        return enum_for(:query_pattern, pattern, **options) unless block_given?
         @nodes = {} # reset cache. FIXME this should probably be in Node.intern
 
         # A pattern graph_name of `false` is used to indicate the default graph
